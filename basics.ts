36479b19c27a39157ab7bc0e001f7d1aa59c645d
Zoran Simonovic <simonovic1980@gmail.com>
2023-06-03 11:09:33 +0200

    functions

--- basics.ts
+++ basics.ts
@@ -1,7 +1,9 @@
+//basic
+
 let userName = "jack";
 let hasLoggedIn = true;
 
-userName += " Herrington";
+userName += " Harrington";
 
 console.log(hasLoggedIn);
 
@@ -9,4 +11,30 @@ let myNumber: number = 10
 
 let myRegex: RegExp= /foo/
 
+//array
+const names: string[] = userName.split(' ')
 
+const numbers: Array<number> = [1, 2, 3]
+
+//object
+
+interface Person  {
+     first: string;
+    last: string;
+}
+
+const myPerson: Person = {
+    first: 'Jack',
+    last: 'Harrington'
+}
+
+const ids: Record<number,string> = {
+    10: 'a',
+    20: 'b'
+}
+
+ids[30] = 'c'
+
+// if statement
+//forEach
+//map
